6ed07a344959a94c7155a8d9d57cce59bdbad47b
MariannaSarkisian <spn.mjj@gmail.com>
2022-09-29 19:32:16 +0300

    example05 ifname

---
+++ example05name/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("enter your name:");
+string username = Console.ReadLine ();
+
+if (username == "Masha")
+{
+    Console.WriteLine("yay Masha!");
+}
+else
+{
+     Console.Write("Hello,");
+Console.WriteLine(username);
+}
