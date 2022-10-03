14e41ad9c224048a2576e2679d058c1209307cfc
MariannaSarkisian <spn.mjj@gmail.com>
2022-10-03 23:47:38 +0300

    after 2 lection c#

---
+++ example10 massiv/Program.cs
@@ -0,0 +1,16 @@
+﻿int [] array = {1,21,32,4,15,4,16,17,8};
+
+int n=array.Length;
+int find=4;
+int index=0;
+
+while (index<n)
+{
+ 
+  if(array[index]==find)
+{
+    Console.WriteLine(index);
+    break;
+}
+  index++;
+}
